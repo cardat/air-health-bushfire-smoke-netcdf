9cad2a5ebdeb0cc72e2da346cfb1cfd06799807f
Ivan Hanigan <ivan.hanigan@gmail.com>
2023-09-01 08:56:38 +0800

    experimenting with stars on z tower

--- R_convert_rds_to_netcdf/do_combine_netcdf_layers.R
+++ R_convert_rds_to_netcdf/do_combine_netcdf_layers.R
@@ -95,8 +95,15 @@ plot(r0[[1:4]])
 r1 <- ifel(r0 > r99, r99, r0)
 outfile <- file.path(rootdir, paste0("bushfiresmoke_v1_3_2001_2020_", var_i, "_trimmed.rds"))
 saveRDS(r1, outfile)
-# writeCDF(r1, filename = outfile)
-stars_r1 <- stars::st_as_stars(r1, ignore_file = T)
+r1 <- readRDS(outfile)
+r1
+outfile <- file.path(rootdir, paste0("bushfiresmoke_v1_3_2001_2020_", var_i, "_trimmed.nc"))
+writeCDF(r1, filename = outfile)
+qc <- terra::rast(outfile)
+
+#stars_r1 <- stars::st_as_stars(r1, ignore_file = T)
+stars_r1 <- stars::st_as_stars(qc, ignore_file = T)
+
 dim(stars_r1)
 names(stars_r1) <- var_i
 stars::write_mdim(stars_r1, file.path(rootdir, paste0("bushfiresmoke_v1_3_2001_2020_",var_i,"_trimmed_2.nc")))
